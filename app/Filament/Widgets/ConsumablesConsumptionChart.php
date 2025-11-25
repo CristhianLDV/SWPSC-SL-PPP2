c3f4a42c3b76ecb5c57fbca9fb30af1ed934d6ba
Cristhian Luis Dominguez Villegas <cdominguezv@ucvvirtual.edu.pe>
2025-11-25 16:36:11 -0500

    Ajustes Finales

--- app/Filament/Widgets/ConsumablesConsumptionChart.php
+++ app/Filament/Widgets/ConsumablesConsumptionChart.php
@@ -24,7 +24,7 @@ class ConsumablesConsumptionChart extends ApexChartWidget
     protected function getOptions(): array
     {
         // Get the last 6 months in a human-readable format.
-        $months = collect()->times(6, function ($number) {
+    /*     $months = collect()->times(6, function ($number) {
             return now()->subMonths($number - 1)->format('F');
         })->reverse()->values()->all();
 
@@ -33,47 +33,62 @@ class ConsumablesConsumptionChart extends ApexChartWidget
             return ConsumablePerson::whereMonth('checked_out_at', now()->subMonths($number - 1)->month)
                 ->whereYear('checked_out_at', now()->subMonths($number - 1)->year)
                 ->count();
-        })->reverse()->values()->all();
+        })->reverse()->values()->all(); */
+         // Forzar Carbon en español
+   
+    // Forzar Carbon en español
+    \Carbon\Carbon::setLocale('es');
 
-        return [
-            'chart' => [
-                'type' => 'area',
-                'height' => 170,
-                'toolbar' => [
-                    'show' => false,
+    // Obtener los últimos 6 meses en español
+    $months = collect()->times(6, function ($number) {
+        return now()->subMonths($number - 1)->translatedFormat('F');
+    })->reverse()->values()->all();
+
+    // Obtener conteo por mes
+    $consumableData = collect()->times(6, function ($number) {
+        return \App\Models\ConsumablePerson::whereMonth(
+                'checked_out_at',
+                now()->subMonths($number - 1)->month
+            )
+            ->whereYear(
+                'checked_out_at',
+                now()->subMonths($number - 1)->year
+            )
+            ->count();
+    })->reverse()->values()->all();
+
+    // Retornar configuración del gráfico
+    return [
+        'chart' => [
+            'type' => 'area',
+            'height' => 170,
+            'toolbar' => ['show' => false],
+        ],
+        'grid' => ['show' => false],
+        'series' => [
+            [
+                'name' => 'Checked Out Consumables',
+                'data' => $consumableData,
+            ],
+        ],
+        'xaxis' => [
+            'categories' => $months,
+            'labels' => [
+                'style' => [
+                    'fontFamily' => 'inherit',
                 ],
             ],
-            'grid' => [
-                'show' => false,
-            ],
-            'series' => [
-                [
-                    'name' => 'Checked Out Consumables',
-                    'data' => $consumableData,
+        ],
+        'yaxis' => [
+            'labels' => [
+                'style' => [
+                    'fontFamily' => 'inherit',
                 ],
             ],
-            'xaxis' => [
-                'categories' => $months,
-                'labels' => [
-                    'style' => [
-                        'fontFamily' => 'inherit',
-                    ],
-                ],
-            ],
-            'yaxis' => [
-                'labels' => [
-                    'style' => [
-                        'fontFamily' => 'inherit',
-                    ],
-                ],
-            ],
-            'colors' => ['#AAB434'],
-            'plotOptions' => [
-                'bar' => [
-                    'borderRadius' => 3,
-                    'horizontal' => true,
-                ],
-            ],
-        ];
+        ],
+        'colors' => ['#AAB434'],
+    ];
+
+
     }
 }
--- app/Filament/Widgets/WidgetThree.php
+++ app/Filament/Widgets/WidgetThree.php
@@ -21,7 +21,7 @@ class WidgetThree extends ApexChartWidget
      * Chart options (series, labels, types, size, animations...)
      * https://apexcharts.com/docs/options
      */
-    protected function getOptions(): array
+    /* protected function getOptions(): array
     {
         // Get the last 6 months in a human-readable format.
         $months = collect()->times(6, function ($number) {
@@ -75,5 +75,66 @@ class WidgetThree extends ApexChartWidget
                 ],
             ],
         ];
-    }
+    } */
+          protected function getOptions(): array
+            {
+                // Forzar Carbon en español
+                \Carbon\Carbon::setLocale('es');
+
+                // Obtener los últimos 6 meses en español
+                $months = collect()->times(6, function ($number) {
+                    return now()->subMonths($number - 1)->translatedFormat('F');
+                })->reverse()->values()->all();
+
+                // Obtener conteo por mes
+                $consumableData = collect()->times(6, function ($number) {
+                    return \App\Models\ComponentHardware::whereMonth(
+                            'checked_out_at',
+                            now()->subMonths($number - 1)->month
+                        )
+                        ->whereYear(
+                            'checked_out_at',
+                            now()->subMonths($number - 1)->year
+                        )
+                        ->count();
+                })->reverse()->values()->all();
+
+                return [
+                    'chart' => [
+                        'type' => 'area',
+                        'height' => 170,
+                        'toolbar' => ['show' => false],
+                    ],
+                    'grid' => ['show' => false],
+                    'series' => [
+                        [
+                            'name' => 'Checked Out Components',
+                            'data' => $consumableData,
+                        ],
+                    ],
+                    'xaxis' => [
+                        'categories' => $months,
+                        'labels' => [
+                            'style' => [
+                                'fontFamily' => 'inherit',
+                            ],
+                        ],
+                    ],
+                    'yaxis' => [
+                        'labels' => [
+                            'style' => [
+                                'fontFamily' => 'inherit',
+                            ],
+                        ],
+                    ],
+                    'colors' => ['#34b487'],
+                    'plotOptions' => [
+                        'bar' => [
+                            'borderRadius' => 3,
+                            'horizontal' => true,
+                        ],
+                    ],
+                ];
+            }
+
 }
